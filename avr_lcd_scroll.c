73acf2202a54e733e20c1f11409c26e4d63b5c8f
dyadem <dyadem@users.noreply.github.com>
2014-11-20 18:30:52 -0800

    Update avr_lcd_scroll.c

--- avr_lcd_scroll.c
+++ avr_lcd_scroll.c
@@ -1,8 +1,15 @@
 /***********************************************
 Sample 02
 CSC 230 Fall 2014 Assignment 03
-'C' code to output scrolling message to AVR microprocessor 
+'C' code to output scrolling message to LCD 
+Display on AVR microprocessor 
 ************************************************/
+
+/*
+CSC 230 Fall 2014 Assignment 3 Part 8
+Jason Sanche
+V00349530
+*/
 
 #include <string.h>
 #include <stdlib.h>
@@ -13,36 +20,20 @@ CSC 230 Fall 2014 Assignment 03
 int main( void )
 {
 	lcd_init();
-	int loopNum = 100;
-	while(loopNum != 0)
-	{
-		int xVal = 0;
-		while(xVal<16)
-		{  		
-  			lcd_xy( xVal, 0 );
-  			lcd_puts( "Display by" );
-			lcd_puts( "                 " );
-			lcd_xy( xVal, 1 );
-  			lcd_puts( "Jason Sanche" );
-			_delay_ms( 250 );
-			lcd_puts( "                 " );
-			xVal++;
-			
+
+	char LINE0[16] = " Display by     ";
+	char LINE1[16] = " Jason Sanche   ";
+
+	for(;;){
+		for(int i = 0; i < 16; i++){
+			for(int j = 0; j < 16; j++){
+				lcd_xy((i+j)%16, 0);
+				lcd_putchar(LINE0[j]);
+				lcd_xy((i+j)%16, 1);
+				lcd_putchar(LINE1[j]);
+			}
+			_delay_ms( 250 );	
 		}
-		loopNum--;
 	}
-#ifdef LCD_LINE2
-  //lcd_xy( 0, 1 );
-  //lcd_puts( " " );
-#ifdef LCD_LINE3
-  lcd_xy( 0, 2 );
-  lcd_puts( "Line 3" );
-#ifdef LCD_LINE4
-  lcd_xy( 0, 3 );
-  lcd_puts( "Line 4" );
-#endif
-#endif  
-#endif  
-  for(;;){
-  }
+
 }
